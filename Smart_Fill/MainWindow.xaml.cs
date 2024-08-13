b43c06aa7c5c28a7e958fb1be4d17d5ec87b8fd5
RajaVigneshB <rajavignesh.b@syncfusion.com>
2024-08-13 11:05:08 +0530

    Addressed the feedbacks of optimizing code

--- Smart_Fill/MainWindow.xaml.cs
+++ Smart_Fill/MainWindow.xaml.cs
@@ -30,8 +30,11 @@ namespace WPFPdfViewer_SmartFill
         public MainWindow()
         {
             InitializeComponent();
+            //Initialize the Semantic Kernel AI
             semanticKernelOpenAI = new SemanticKernelAI("YOUR-AI-KEY");
+            //Load the PDF document in the PDF Viewer
             pdfViewer.Load("../../../Data/form_document_new.pdf");
+            //Zoom the PDF Viewer to 50% to view the entire page
             pdfViewer.ZoomTo(50);
         }
         #endregion
@@ -46,51 +49,10 @@ namespace WPFPdfViewer_SmartFill
         {
             // Get the text search stack panel from the toolbar of the PDF Viewer
             DocumentToolbar toolbar = (DocumentToolbar)pdfViewer.Template.FindName("PART_Toolbar", pdfViewer);
-            StackPanel textSeacrchStack = (StackPanel)toolbar.Template.FindName("PART_TextSearchStack", toolbar);
-            ToggleButton annotationToggleButton = (ToggleButton)toolbar.Template.FindName("PART_Annotations", toolbar);
-            if (textSeacrchStack != null)
-            {
-                // Add AI Assist button to the text search stack of the toolbar
-                aIAssistButton = new ToggleButton();
-                TextBlock aIAssistText = new TextBlock();
-                aIAssistText.Text = "Smart Fill";
-                aIAssistText.FontSize = 14;
-                aIAssistButton.Content = aIAssistText;
-                aIAssistButton.Checked += AIAssistButton_Checked;
-                aIAssistButton.Unchecked += AIAssistButton_Unchecked; ;
-                aIAssistButton.Height = 32;
-                aIAssistButton.Margin = new Thickness(0, 0, 8, 0);
-                aIAssistButton.Padding = new Thickness(4);
-                // Set the style of the AI Assist button
-                if (annotationToggleButton != null)
-                {
-                    aIAssistButton.Style = annotationToggleButton.Style;
-                }
-                if (textSeacrchStack.Children != null && textSeacrchStack.Children.Count > 0)
-                {
-                    textSeacrchStack.Children.Insert(0, aIAssistButton);
-                }
-                else
-                {
-                    textSeacrchStack.Children.Add(aIAssistButton);
-                }
-
-                //Create the linear gradient brush for the loading indicator
-                LinearGradientBrush linearGradientBrush = new LinearGradientBrush();
-                linearGradientBrush.StartPoint = new System.Windows.Point(0.5, 0);
-                linearGradientBrush.EndPoint = new System.Windows.Point(0.5, 1);
-                linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xFF, 0xFE, 0xFE, 0xFE), 0.027));
-                linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xFF, 0xFE, 0xFE, 0xFE), 0.029));
-                linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xFF, 0xF0, 0xF0, 0xF0), 0.498));
-                linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xE2, 0xE2, 0xE2, 0xE2), 0.966));
-                linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xE2, 0xE2, 0xE2, 0xE2), 0.968));
-
-                //Set the background and foreground for the buttons
-                aI_Title.Background = linearGradientBrush;
-                aI_Title.Foreground = aIAssistText.Foreground;
-                smartFillButton.Background = linearGradientBrush;
-                smartFillButton.Foreground = aIAssistText.Foreground;
-            }
+            // Collapse the open menu in the toolbar
+            CollapseOpenMenu(toolbar);
+            // Add the AI Assistance button to the toolbar
+            AddAIAssistanceButton(toolbar);
         }
 
         /// <summary>
@@ -169,35 +131,12 @@ namespace WPFPdfViewer_SmartFill
             //Display the loading indicator
             loadingCanvas.Visibility = Visibility.Visible;
             loadingIndicator.Visibility = Visibility.Visible;
-            // Get the selected user details from the combo box
-            string selectedUserDetails = userDetailsTextBox.Text;
-            // Get the XFDF file content from the PDF Viewer
-            string loadedFileDetails = ExportFormDetails();
-            // Get the hint values for the form fields
-            string CustomValues = HintValuesforFields();
-            // Create a prompt message for the semantic kernel AI
-            string mergePrompt = $"Merge the input data into the XFDF file content. Hint text: {CustomValues}. " +
-                            $"Ensure the input data matches suitable field names. " +
-                            $"Here are the details: " +
-                            $"input data: {selectedUserDetails}, " +
-                            $"XFDF information: {loadedFileDetails}. " +
-                            $"Provide the resultant XFDF directly. " +
-                            $"Some conditions need to follow: " +
-                            $"1. input data is not directly provided as the field name; you need to think and merge appropriately. " +
-                            $"2. When comparing input data and field names, ignore case sensitivity. " +
-                            $"3. First, determine the best match for the field name. If there isn’t an exact match, use the input data to find a close match. " +
-                            $"remove ```xml and remove ``` if it is there in the code.";
+
             // Get the answer from GPT using the semantic kernel AI
-            string result = await semanticKernelOpenAI.GetAnswerFromGPT( mergePrompt);
-            var inputFileStream = new MemoryStream();
-            // Write the merged XFDF content to the MemoryStream
-            var writer = new StreamWriter(inputFileStream, leaveOpen: true);
-            await writer.WriteAsync(result);
-            await writer.FlushAsync();
-            inputFileStream.Position = 0;
-            byte[] formData = new byte[inputFileStream.Length];
-            inputFileStream.Read(formData, 0, formData.Length);
-            pdfViewer.ImportFormData(formData, Syncfusion.Pdf.Parsing.DataFormat.XFdf);
+            string result = await GetDataFromAI();
+            //Import the data to the PDF Viewer
+            ImportDataToPdfViewer(result);
+
             // Hide the loading indicator
             loadingCanvas.Visibility = Visibility.Collapsed;
             loadingIndicator.Visibility = Visibility.Collapsed;
@@ -310,6 +249,134 @@ namespace WPFPdfViewer_SmartFill
             userDetailsTextBox.Text = string.Empty;
             userDetailsComboBox.SelectedIndex = -1;
             pdfViewer.Focus();
+        }
+
+        /// <summary>
+        /// Collapse the open menu in the toolbar.
+        /// </summary>
+        /// <param name="toolbar">Toolbar of the PdfViewer</param>
+        private void CollapseOpenMenu(DocumentToolbar toolbar)
+        {
+            //Get the instance of the file menu button using its template name.
+            ToggleButton FileButton = (ToggleButton)toolbar.Template.FindName("PART_FileToggleButton", toolbar);
+            //Get the instance of the file menu button context menu and the item collection.
+            ContextMenu FileContextMenu = FileButton.ContextMenu;
+            foreach (MenuItem FileMenuItem in FileContextMenu.Items)
+            {
+                //Get the instance of the open menu item using its template name and disable its visibility.
+                if (FileMenuItem.Name == "PART_OpenMenuItem")
+                    FileMenuItem.Visibility = System.Windows.Visibility.Collapsed;
+            }
+        }
+
+        /// <summary>
+        /// Add the AI Assistance button to the toolbar.
+        /// </summary>
+        /// <param name="toolbar">Toolbar of the PdfViewer</param>
+        private void AddAIAssistanceButton(DocumentToolbar toolbar)
+        {
+            // Get the text search stack panel and annotation button from the toolbar
+            StackPanel textSeacrchStack = (StackPanel)toolbar.Template.FindName("PART_TextSearchStack", toolbar);
+            ToggleButton annotationToggleButton = (ToggleButton)toolbar.Template.FindName("PART_Annotations", toolbar);
+            //Create a new toggle button for AI Assist
+            aIAssistButton = new ToggleButton();
+            TextBlock aIAssistText = new TextBlock();
+            aIAssistText.Text = "Smart Fill";
+            aIAssistText.FontSize = 14;
+            aIAssistButton.Content = aIAssistText;
+            aIAssistButton.Checked += AIAssistButton_Checked;
+            aIAssistButton.Unchecked += AIAssistButton_Unchecked; ;
+            aIAssistButton.Height = 32;
+            aIAssistButton.Margin = new Thickness(0, 0, 8, 0);
+            aIAssistButton.Padding = new Thickness(4);
+            // Set the style of the AI Assist button
+            if (annotationToggleButton != null)
+            {
+                aIAssistButton.Style = annotationToggleButton.Style;
+            }
+            // Add AI Assist button to the text search stack of the toolbar
+            if (textSeacrchStack.Children != null && textSeacrchStack.Children.Count > 0)
+            {
+                textSeacrchStack.Children.Insert(0, aIAssistButton);
+            }
+            else
+            {
+                textSeacrchStack.Children.Add(aIAssistButton);
+            }
+
+            ApplyColorToButtons(aIAssistText.Foreground);
+        }
+
+        /// <summary>
+        /// Apply the color to the buttons in the toolbar.
+        /// </summary>
+        /// <param name="foregroundBrush">Fore ground color</param>
+        private void ApplyColorToButtons(Brush foregroundBrush)
+        {
+            //Create the linear gradient brush for the loading indicator
+            LinearGradientBrush linearGradientBrush = new LinearGradientBrush();
+            linearGradientBrush.StartPoint = new System.Windows.Point(0.5, 0);
+            linearGradientBrush.EndPoint = new System.Windows.Point(0.5, 1);
+            linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xFF, 0xFE, 0xFE, 0xFE), 0.027));
+            linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xFF, 0xFE, 0xFE, 0xFE), 0.029));
+            linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xFF, 0xF0, 0xF0, 0xF0), 0.498));
+            linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xE2, 0xE2, 0xE2, 0xE2), 0.966));
+            linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xE2, 0xE2, 0xE2, 0xE2), 0.968));
+
+            //Set the background and foreground for the buttons
+            aI_Title.Background = linearGradientBrush;
+            aI_Title.Foreground = foregroundBrush;
+            smartFillButton.Background = linearGradientBrush;
+            smartFillButton.Foreground = foregroundBrush;
+        }
+
+        /// <summary>
+        /// Import the data to the PDF Viewer.
+        /// </summary>
+        /// <param name="resultData">Result from the AI</param>
+        private async void ImportDataToPdfViewer(string resultData)
+        {
+            var inputFileStream = new MemoryStream();
+            // Write the merged XFDF content to the MemoryStream
+            var writer = new StreamWriter(inputFileStream, leaveOpen: true);
+            await writer.WriteAsync(resultData);
+            await writer.FlushAsync();
+            inputFileStream.Position = 0;
+            byte[] formData = new byte[inputFileStream.Length];
+            inputFileStream.Read(formData, 0, formData.Length);
+            pdfViewer.ImportFormData(formData, Syncfusion.Pdf.Parsing.DataFormat.XFdf);
+        }
+        #endregion
+
+        #region OpenAI
+        /// <summary>
+        /// Method to get the answer from GPT using the semantic kernel AI
+        /// </summary>
+        /// <returns>Returns the data to be imported in pdfViewer</returns>
+        private async Task<string> GetDataFromAI()
+        {
+            string result = string.Empty;
+            // Get the selected user details from the combo box
+            string selectedUserDetails = userDetailsTextBox.Text;
+            // Get the XFDF file content from the PDF Viewer
+            string loadedFileDetails = ExportFormDetails();
+            // Get the hint values for the form fields
+            string CustomValues = HintValuesforFields();
+            // Create a prompt message for the semantic kernel AI
+            string mergePrompt = $"Merge the input data into the XFDF file content. Hint text: {CustomValues}. " +
+                            $"Ensure the input data matches suitable field names. " +
+                            $"Here are the details: " +
+                            $"input data: {selectedUserDetails}, " +
+                            $"XFDF information: {loadedFileDetails}. " +
+                            $"Provide the resultant XFDF directly. " +
+                            $"Some conditions need to follow: " +
+                            $"1. input data is not directly provided as the field name; you need to think and merge appropriately. " +
+                            $"2. When comparing input data and field names, ignore case sensitivity. " +
+                            $"3. First, determine the best match for the field name. If there isn’t an exact match, use the input data to find a close match. " +
+                            $"remove ```xml and remove ``` if it is there in the code.";
+            // Get the answer from GPT using the semantic kernel AI
+            result = await semanticKernelOpenAI.GetAnswerFromGPT(mergePrompt);
+            return result;
         }
         #endregion
     }
