7ad45708e44a8e02457ec53ce4be5bc0c3aee10f
RajaVigneshB <rajavignesh.b@syncfusion.com>
2024-08-21 15:00:05 +0530

    Sample for smart Summarization in WPF PdfViewer

--- Smart_Summarize/MainWindow.xaml.cs
+++ Smart_Summarize/MainWindow.xaml.cs
@@ -1,15 +1,10 @@
 ﻿using Syncfusion.Windows.PdfViewer;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Syncfusion.Windows.Shared;
 
 namespace Smart_Summarize
 {
@@ -20,17 +15,28 @@ namespace Smart_Summarize
     {
         #region Fields
         private SemanticKernelAI semanticKernelOpenAI;
-        ToggleButton aIAssistButton;
+        private ToggleButton aIAssistButton;
+        private UserChatBox userInputChat;
+        private AIResultChatBox aiResultChat;
+        private ChatTextBlock chatText;
         #endregion
 
         #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+            pdfViewer.Load("../../../Data/GIS Succinctly.pdf");
+            sendIcon.Source = new BitmapImage(new Uri("../../../Data/send_icon.png", UriKind.Relative));
+            semanticKernelOpenAI = new SemanticKernelAI("YOUR-AI-KEY");
         }
         #endregion
 
         #region Event Handlers
+        /// <summary>
+        /// Indicate the document is loaded in the PdfViewer
+        /// </summary>
+        /// <param name="sender">PdfViewer Control</param>
+        /// <param name="args">Event arguments</param>
         private void pdfViewer_DocumentLoaded(object sender, EventArgs args)
         {
             //Collapse the AI Assistance when the document is loaded
@@ -38,6 +44,9 @@ namespace Smart_Summarize
             {
                 aIAssistButton.IsChecked = false;
             }
+
+            //Clear the chat stack panel when the document is loaded
+            chatStack.Children.Clear();
         }
 
         /// <summary>
@@ -53,14 +62,80 @@ namespace Smart_Summarize
             // Add the AI Assistance button to the toolbar
             AddAIAssistanceButton(toolbar);
         }
-        private void AIAssistButton_Unchecked(object sender, RoutedEventArgs e)
-        {
 
+        private void inputText_GotFocus(object sender, RoutedEventArgs e)
+        {
+            //Add the default prompt to the input text box
+            inputText.Text = "";
+            SolidColorBrush textForeGround = inputText.Foreground as SolidColorBrush;
+            if (textForeGround != null)
+            {
+                //Make the text color half transparent for the default prompt
+                inputText.Foreground = new SolidColorBrush(Color.FromArgb((byte)((int)textForeGround.Color.A * 2), textForeGround.Color.R, textForeGround.Color.G, textForeGround.Color.B));
+            }
         }
 
-        private void AIAssistButton_Checked(object sender, RoutedEventArgs e)
+        private void inputText_LostFocus(object sender, RoutedEventArgs e)
         {
+            if (inputText.Text.Length <= 0)
+            {
+                AddDefaultPromptToChatBox();
+            }
+        }
+        private void AIAssistButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            //Collapse the AI Assistance when the button is unchecked
+            summarizeGrid.Visibility = Visibility.Collapsed;
+            pdfViewer.Focus();
+        }
 
+        private async void AIAssistButton_Checked(object sender, RoutedEventArgs e)
+        {
+            //Expand the AI Assistance when the button is checked
+            summarizeGrid.Visibility = Visibility.Visible;
+            inputText.Text = string.Empty;
+            //Add the default prompt to the input text box
+            AddDefaultPromptToChatBox();
+            if (chatStack.Children.Count == 0)
+            {
+                //Add the initial user chat question to the chat stack
+                AddUserChat("Summarize this PDF document");
+
+                loadingCanvas.Visibility = Visibility.Visible;
+                loadingIndicator.Header = "Summarizing the PDF...";
+                loadingIndicator.Visibility = Visibility.Visible;
+                //Extract the text from the PDF document
+                await ExtractDetailsFromPDF();
+
+                //Summarize the PDF when the chat stack is empty
+                string summaryText = await SummarizePDF();
+
+                //Display the summarized text in the AI Result TextBlock
+                AddAIChat(summaryText);
+                loadingCanvas.Visibility = Visibility.Collapsed;
+                loadingIndicator.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(inputText.Text))
+            {
+                //Add the user input to the chat stack
+                AddUserChat(inputText.Text);
+                inputText.Text = string.Empty;
+                AddDefaultPromptToChatBox();
+
+                loadingCanvas.Visibility = Visibility.Visible;
+                loadingIndicator.Header = "Reviewing Question...";
+                loadingIndicator.Visibility = Visibility.Visible;
+                //Get the answer from GPT using the semantic kernel for the user input
+                string answer = await semanticKernelOpenAI.AnswerQuestion(chatText.Text);
+                //Display the answer in the AI Result TextBlock
+                AddAIChat(answer);
+                loadingCanvas.Visibility = Visibility.Collapsed;
+                loadingIndicator.Visibility = Visibility.Collapsed;
+            }
         }
         #endregion
 
@@ -77,7 +152,7 @@ namespace Smart_Summarize
             //Create a new toggle button for AI Assist
             aIAssistButton = new ToggleButton();
             TextBlock aIAssistText = new TextBlock();
-            aIAssistText.Text = "AI Summarizer";
+            aIAssistText.Text = "AI Assist View";
             aIAssistText.FontSize = 14;
             aIAssistButton.Content = aIAssistText;
             aIAssistButton.Checked += AIAssistButton_Checked;
@@ -100,28 +175,144 @@ namespace Smart_Summarize
                 textSeacrchStack.Children.Add(aIAssistButton);
             }
 
-            ApplyColorToButtons(aIAssistText.Foreground);
+            ApplyColorToButtons(aIAssistText.Foreground, toolbar);
         }
 
         /// <summary>
         /// Apply the color to the buttons in the toolbar.
         /// </summary>
         /// <param name="foregroundBrush">Fore ground color</param>
-        private void ApplyColorToButtons(Brush foregroundBrush)
+        private void ApplyColorToButtons(Brush foregroundBrush, DocumentToolbar toolbar)
         {
-            //Create the linear gradient brush for the loading indicator
-            LinearGradientBrush linearGradientBrush = new LinearGradientBrush();
-            linearGradientBrush.StartPoint = new System.Windows.Point(0.5, 0);
-            linearGradientBrush.EndPoint = new System.Windows.Point(0.5, 1);
-            linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xFF, 0xFE, 0xFE, 0xFE), 0.027));
-            linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xFF, 0xFE, 0xFE, 0xFE), 0.029));
-            linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xFF, 0xF0, 0xF0, 0xF0), 0.498));
-            linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xE2, 0xE2, 0xE2, 0xE2), 0.966));
-            linearGradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromArgb(0xE2, 0xE2, 0xE2, 0xE2), 0.968));
-
+            // Retrieve the root element of the template
+            var rootElement = VisualTreeHelper.GetChild(toolbar, 0) as FrameworkElement;
+            Brush background = Brushes.Transparent;
+            if (rootElement != null)
+            {
+                // Traverse the visual tree to find the first Border
+                var border = FindVisualChild<Border>(rootElement);
+                if (border != null && border.Name != "Part_AnnotationToolbar")
+                {
+                    background = border.Background;
+                }
+            }
             //Set the background and foreground for the buttons
-            aI_Title.Background = linearGradientBrush;
+            aI_Title.Background = background;
             aI_Title.Foreground = foregroundBrush;
+            sendButton.Background = background;
+            sendButton.Foreground = foregroundBrush;
+        }
+
+        /// <summary>
+        /// Add the default prompt to the chat box.
+        /// </summary>
+        private void AddDefaultPromptToChatBox()
+        {
+            //Add the default prompt to the input text box
+            inputText.Text = "Type your prompt for assistance...";
+            SolidColorBrush textForeGround = inputText.Foreground as SolidColorBrush;
+            if (textForeGround != null)
+            {
+                //Make the text color half transparent for the default prompt
+                inputText.Foreground = new SolidColorBrush(Color.FromArgb((byte)((int)textForeGround.Color.A / 2), textForeGround.Color.R, textForeGround.Color.G, textForeGround.Color.B));
+            }
+        }
+
+        /// <summary>
+        /// Method to find the visual child of the parent element.
+        /// </summary>
+        /// <typeparam name="T">Type of the child</typeparam>
+        /// <param name="parent">Parent element</param>
+        /// <returns>Returns the specified type of child</returns>
+        private T FindVisualChild<T>(DependencyObject parent) where T : DependencyObject
+        {
+            if (parent != null && VisualTreeHelper.GetChildrenCount(parent) > 0)
+            {
+                // Traverse the visual tree to find the first child of the specified type
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+                {
+                    var child = VisualTreeHelper.GetChild(parent, i);
+                    // Check if the child is of the specified type
+                    if (child is T tChild)
+                    {
+                        return tChild;
+                    }
+
+                    // Recursively search the child elements
+                    var result = FindVisualChild<T>(child);
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Extract the text details from the PDF document.
+        /// </summary>
+        /// <returns>Returns nothing</returns>
+        private async Task ExtractDetailsFromPDF()
+        {
+            List<string> extractedText = new List<string>();
+            Syncfusion.Pdf.TextLines textLines = new Syncfusion.Pdf.TextLines();
+            //Extract the text from the PDF document
+            for (int pageIndex = 0; pageIndex < pdfViewer.PageCount; pageIndex++)
+            {
+                string text = $"... Page {pageIndex + 1} ...\n";
+                text += pdfViewer.ExtractText(pageIndex, out textLines);
+                extractedText.Add(text);
+            }
+
+            await semanticKernelOpenAI.CreateEmbeddedPage(extractedText.ToArray());
+        }
+
+        /// <summary>
+        /// Summarize the PDF document with the extracted text using the Semantic Kernel AI.
+        /// </summary>
+        /// <returns>Returns the summarized content as string</returns>
+        private async Task<string> SummarizePDF()
+        {
+            //Summarize the text using the Semantic Kernel AI
+            string summary = await semanticKernelOpenAI.GetAnswerFromGPT("You are a helpful assistant. Your task is to analyze the provided text and generate short summary.");
+            return summary;
+        }
+        #endregion
+
+        #region Chat Creation Methods
+        /// <summary>
+        /// Add the AI chat to the chat stack.
+        /// </summary>
+        /// <param name="text">Text to be added</param>
+        private void AddAIChat(string text)
+        {
+            chatText = new ChatTextBlock();
+            chatText.ApplyStyle();
+            chatText.Text = text;
+            aiResultChat = new AIResultChatBox();
+            aiResultChat.ApplyStyle();
+            aiResultChat.Child = chatText;
+            chatStack.Children.Add(aiResultChat);
+            aiResultChat.BringIntoView();
+        }
+
+        /// <summary>
+        /// Add the user chat to the chat stack.
+        /// </summary>
+        /// <param name="text">Text to be added</param>
+        private void AddUserChat(string text)
+        {
+            chatText = new ChatTextBlock();
+            chatText.ApplyStyle();
+            chatText.Text = text;
+            userInputChat = new UserChatBox();
+            string skinManagerStyle = SfSkinManagerExtension.GetBaseThemeName(this);
+            userInputChat.Applystyle(skinManagerStyle);
+            userInputChat.Child = chatText;
+            chatStack.Children.Add(userInputChat);
+            userInputChat.BringIntoView();
         }
         #endregion
     }
--- Smart_Summarize/SemanticKernelAI.cs
+++ Smart_Summarize/SemanticKernelAI.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel;
+using System.Text;
+using SmartComponents.LocalEmbeddings;
 
 namespace Smart_Summarize
 {
@@ -13,6 +15,8 @@ namespace Smart_Summarize
 
         IChatCompletionService chatCompletionService;
         Kernel kernel;
+
+        public Dictionary<string, EmbeddingF32>? PageEmbeddings { get; set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SemanticKernelAI"/> class.
@@ -27,14 +31,27 @@ namespace Smart_Summarize
         }
 
         /// <summary>
+        /// Create the embedded page from the extracted chunks in the PDF
+        /// </summary>
+        /// <param name="chunks">Extracted text from pdfViewer</param>
+        /// <returns></returns>
+        public async Task CreateEmbeddedPage(string[] chunks)
+        {
+            var embedder = new LocalEmbedder();
+            PageEmbeddings = chunks.Select(x => KeyValuePair.Create(x, embedder.Embed(x))).ToDictionary(k => k.Key, v => v.Value);
+        }
+
+        /// <summary>
         /// Method to get the answer from GPT using the semantic kernel
         /// </summary>
         /// <param name="systemPrompt">Prompt for the system message</param>
         /// <returns>Returns the form data as a string</returns>
         public async Task<string> GetAnswerFromGPT(string systemPrompt)
         {
+            List<string> message = PageEmbeddings.Keys.Take(10).ToList();
             var history = new ChatHistory();
             history.AddSystemMessage(systemPrompt);
+            history.AddUserMessage(string.Join(" ", message));
             OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
             {
                 ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
@@ -45,6 +62,45 @@ namespace Smart_Summarize
             kernel: kernel);
 
             return result.ToString();
+        }
+
+        /// <summary>
+        /// Method to get the answer from GPT using the semantic kernel
+        /// </summary>
+        /// <param name="systemPrompt">Prompt for the system message</param>
+        /// <param name="userText">User text for the AI</param>
+        /// <returns>Returns the form data as a string</returns>
+        public async Task<string> GetAnswerFromGPT(string systemPrompt, string userText)
+        {
+            var history = new ChatHistory();
+            history.AddSystemMessage(systemPrompt);
+            history.AddUserMessage(userText);
+            OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+            {
+                ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+            };
+            var result = await chatCompletionService.GetChatMessageContentAsync(
+            history,
+            executionSettings: openAIPromptExecutionSettings,
+            kernel: kernel);
+
+            return result.ToString();
+        }
+
+        public async Task<string> AnswerQuestion(string question)
+        {
+            var embedder = new LocalEmbedder();
+            var questionEmbedding = embedder.Embed(question);
+            var results = LocalEmbedder.FindClosestWithScore(questionEmbedding, PageEmbeddings.Select(x => (x.Key, x.Value)), 5, 0.5f);
+            StringBuilder builder = new StringBuilder();
+            foreach (var result in results)
+            {
+                builder.AppendLine(result.Item);
+            }
+            string message = builder.ToString();
+            var answer = await GetAnswerFromGPT("You are a helpful assistant. Use the provided PDF document pages and pick a precise page to answer the user question, proivde a reference at the bottom of the content with page numbers like ex: Reference: [20,21,23]. Pages: " + message, question);
+
+            return answer;
         }
     }
 }
