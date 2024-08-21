0c4c5b7347840d8d16ab1b10c1a460f7f5e9ec71
RajaVigneshB <rajavignesh.b@syncfusion.com>
2024-08-21 15:03:44 +0530

    Added the missed files

---
+++ Smart_Summarize/UserChatBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Syncfusion.Windows.Shared;
+
+namespace Smart_Summarize
+{
+    internal class UserChatBox : Border
+    {
+        static UserChatBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(UserChatBox), new FrameworkPropertyMetadata(typeof(UserChatBox)));
+        }
+
+
+        public void Applystyle(string skinManagerStyle)
+        {
+            this.HorizontalAlignment = HorizontalAlignment.Right;
+            this.MaxWidth = 300;
+            this.CornerRadius = new CornerRadius(5);
+            this.BorderThickness = new Thickness(2);
+            this.Margin = new Thickness(10);
+            if (skinManagerStyle != null && !string.IsNullOrEmpty(skinManagerStyle.ToString()) && skinManagerStyle.ToString().ToLower().Contains("windows11"))
+            {
+                this.SetResourceReference(Button.BackgroundProperty, "ContentBackgroundHovered");
+                this.SetResourceReference(Button.ForegroundProperty, "IconColorHovered");
+            }
+            else
+            {
+                this.SetResourceReference(Button.BackgroundProperty, "SecondaryBackgroundHovered");
+                this.SetResourceReference(Button.ForegroundProperty, "SecondaryForegroundHovered");
+            }
+        }
+    }
+}
